1a8e7339055492fcdc9c03742cd62d16b3b30364
Dave Stanton <davestanton.us@gmail.com>
2016-05-02 22:03:01 -0400

    Red everywhere.

--- AutoLayoutContstraints/AutoLayoutContstraints/ViewController.swift
+++ AutoLayoutContstraints/AutoLayoutContstraints/ViewController.swift
@@ -16,11 +16,21 @@ class ViewController: UIViewController {
         // we create a simple UIView
         
         let redBox = UIView()
+        
         redBox.backgroundColor = UIColor.redColor()
         redBox.topAnchor.constraintEqualToAnchor(view.topAnchor)
         redBox.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor)
         redBox.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
         redBox.rightAnchor.constraintEqualToAnchor(view.rightAnchor)
+        redBox.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(redBox)
+        
+        redBox.topAnchor.constraintEqualToAnchor(view.topAnchor).active = true
+        redBox.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor).active = true
+        redBox.leftAnchor.constraintEqualToAnchor(view.leftAnchor).active = true
+        redBox.rightAnchor.constraintEqualToAnchor(view.rightAnchor).active = true
+ 
     }
 
     override func didReceiveMemoryWarning() {
